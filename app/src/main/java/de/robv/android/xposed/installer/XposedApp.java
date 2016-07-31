2b3aecaab918036db31bbdd7bd846c63890b7d6d
dvdandroid <davide.intelligent@gmail.com>
2016-07-31 15:18:02 +0200

    Bump version

--- app/src/main/java/de/robv/android/xposed/installer/XposedApp.java
+++ app/src/main/java/de/robv/android/xposed/installer/XposedApp.java
@@ -55,7 +55,7 @@ public class XposedApp extends Application implements ActivityLifecycleCallbacks
     private static final File XPOSED_PROP_FILE_SYSTEMLESS_3 = new File("/su/xposed/system/xposed.prop");
     private static final File XPOSED_PROP_FILE = new File("/system/xposed.prop");
     public static int WRITE_EXTERNAL_PERMISSION = 69;
-    public static String THIS_APK_VERSION = "1469115000001";
+    public static String THIS_APK_VERSION = "1469970000000";
     public static int[] iconsValues = new int[]{R.mipmap.ic_launcher, R.mipmap.ic_launcher_hjmodi, R.mipmap.ic_launcher_rovo, R.mipmap.ic_launcher_rovo_old, R.mipmap.ic_launcher_staol};
     private static Pattern PATTERN_APP_PROCESS_VERSION = Pattern.compile(".*with Xposed support \\(version (.+)\\).*");
     private static XposedApp mInstance = null;
