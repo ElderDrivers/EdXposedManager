a3c1279a1feff4a199090d35efdfff6dcb2378d9
dvdandroid <davide.intelligent@gmail.com>
2016-10-28 21:30:20 +0200

    Fix FC

--- app/src/main/java/de/robv/android/xposed/installer/util/NavUtil.java
+++ app/src/main/java/de/robv/android/xposed/installer/util/NavUtil.java
@@ -19,7 +19,12 @@ public final class NavUtil {
             return null;
 
         Spannable spannable = new SpannableString(str);
-        Linkify.addLinks(spannable, Linkify.ALL);
+        try {
+            Linkify.addLinks(spannable, Linkify.ALL);
+        } catch (Exception e) {
+            return null;
+        }
+
         URLSpan spans[] = spannable.getSpans(0, spannable.length(), URLSpan.class);
         return (spans.length > 0) ? Uri.parse(spans[0].getURL()) : null;
     }
