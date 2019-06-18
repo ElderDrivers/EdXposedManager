9390b49cd0297d67dd3278f162e8d51fd3e41374
neoblackxt <neoblackxt@outlook.com>
2019-06-18 20:00:33 +0800

    typo

--- app/src/main/java/de/robv/android/xposed/installer/util/LocaleUtil.java
+++ app/src/main/java/de/robv/android/xposed/installer/util/LocaleUtil.java
@@ -7,7 +7,7 @@ import android.content.res.Resources;
 import java.util.Locale;
 
 public class LocaleUtil {
-    public static void setLacale(Context context, Locale locale) {
+    public static void setLocale(Context context, Locale locale) {
         Resources resources = context.getResources();
         Configuration configuration = resources.getConfiguration();
         configuration.setLocale(locale);
