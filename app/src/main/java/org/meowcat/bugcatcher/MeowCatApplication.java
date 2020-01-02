852eefa2647649dcb4a9d30e83df038226d39bfe
Jim Wu <mlgmxyysd@meowcat.org>
2020-01-02 21:21:20 +0800

    Remove hidden api warning

--- app/src/main/java/org/meowcat/bugcatcher/MeowCatApplication.java
+++ app/src/main/java/org/meowcat/bugcatcher/MeowCatApplication.java
@@ -4,6 +4,12 @@
 
 package org.meowcat.bugcatcher;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import de.robv.android.xposed.installer.XposedApp;
 
 public class MeowCatApplication extends XposedApp {
@@ -14,5 +20,21 @@ public class MeowCatApplication extends XposedApp {
         super.onCreate();
         CrashHandler crashHandler = CrashHandler.getInstance();
         crashHandler.init(getApplicationContext());
+        disableAPIDialog();
+    }
+    private void disableAPIDialog(){
+        if (Build.VERSION.SDK_INT < 28) {
+            return;
+        }
+        try {
+            @SuppressLint({"PrivateApi", "DiscouragedPrivateApi"}) Method currentActivityThread = Class.forName("android.app.ActivityThread").getDeclaredMethod("currentActivityThread");
+            currentActivityThread.setAccessible(true);
+            Object activityThread = currentActivityThread.invoke(null);
+            @SuppressLint("PrivateApi") Field mHiddenApiWarningShown = Class.forName("android.app.ActivityThread").getDeclaredField("mHiddenApiWarningShown");
+            mHiddenApiWarningShown.setAccessible(true);
+            mHiddenApiWarningShown.setBoolean(activityThread, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
