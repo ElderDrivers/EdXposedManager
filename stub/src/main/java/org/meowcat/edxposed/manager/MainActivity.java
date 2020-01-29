56547eb35bcdb7fd75a738adeea987cff0afc3a9
Jim Wu <mlgmxyysd@meowcat.org>
2020-01-29 21:57:33 +0800

    Generating stub for module

---
+++ stub/src/main/java/org/meowcat/edxposed/manager/MainActivity.java
@@ -0,0 +1,44 @@
+package org.meowcat.edxposed.manager;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Browser;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.URLSpan;
+import android.text.util.Linkify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setTitle(R.string.app_name)
+                .setMessage(R.string.upgrade_msg)
+                .setPositiveButton(R.string.btn_ok, (d, w) -> {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, parseURL("https://github.com/ElderDrivers/EdXposedManager/releases/latest"));
+                    intent.putExtra(Browser.EXTRA_APPLICATION_ID, getPackageName());
+                    startActivity(intent);
+                    finish();
+                })
+                .setNegativeButton(R.string.btn_no, (d, w) -> finish())
+                .show();
+    }
+
+    public static Uri parseURL(String str) {
+        if (str == null || str.isEmpty())
+            return null;
+
+        Spannable spannable = new SpannableString(str);
+        Linkify.addLinks(spannable, Linkify.WEB_URLS | Linkify.EMAIL_ADDRESSES);
+
+        URLSpan[] spans = spannable.getSpans(0, spannable.length(), URLSpan.class);
+        return (spans.length > 0) ? Uri.parse(spans[0].getURL()) : null;
+    }
+}
