eefca8ef641be34b7247b84a88f4caabbac3d30e
高渐离 <admin@gaojianli.me>
2021-12-15 13:22:00 +0800

    Add codename for API31

--- app/src/main/java/org/meowcat/edxposed/manager/StatusInstallerFragment.java
+++ app/src/main/java/org/meowcat/edxposed/manager/StatusInstallerFragment.java
@@ -322,6 +322,8 @@ public class StatusInstallerFragment extends Fragment {
                 return "Quince Tart";
             case 30:
                 return "Red Velvet Cake";
+            case 31:
+				return "Snow Cone";
         }
         return "Unknown";
     }
@@ -334,7 +336,7 @@ public class StatusInstallerFragment extends Fragment {
         manufacturer += " " + Build.MODEL + " ";
         if (new File("/system/framework/framework-miui-res.apk").exists() || new File("/system/app/miui/miui.apk").exists() || new File("/system/app/miuisystem/miuisystem.apk").exists()) {
             manufacturer += "(MIUI)";
-        } else if (new File("/system/priv-app/oneplus-framework-res/oneplus-framework-res.apk").exists()) {
+        } else if (new File("/system/priv-app/oneplus-framework-res/oneplus-framework-res.apk").exists() || new File("/system/framework/oplus-framework.jar").exists()) {
             manufacturer += "(Hydrogen/Oxygen OS)";
         } else if (new File("/system/framework/oppo-framework.jar").exists() || new File("/system/framework/oppo-framework-res.apk").exists() || new File("/system/framework/coloros-framework.jar").exists() || new File("/system/framework/coloros.services.jar").exists() || new File("/system/framework/oppo-services.jar").exists() || new File("/system/framework/coloros-support-wrapper.jar").exists()) {
             manufacturer += "(Color OS)";
